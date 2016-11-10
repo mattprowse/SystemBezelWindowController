0fba6421a7f5e8459acb1cfb751a9d44bcd45238
Matt Prowse <matt@cordlessdog.com>
2016-11-11 10:12:56 +1100

    Initial Commit

---
+++ SystemBezelWindowController/SystemBezelWindowController.h
@@ -0,0 +1,19 @@
+//
+//  SystemBezelWindowController.h
+//  SystemBezelWindowController
+//
+//  Created by Matt Prowse on 11/11/16.
+//  Copyright © 2016 Cordless Dog. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SystemBezelWindowController.
+FOUNDATION_EXPORT double SystemBezelWindowControllerVersionNumber;
+
+//! Project version string for SystemBezelWindowController.
+FOUNDATION_EXPORT const unsigned char SystemBezelWindowControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SystemBezelWindowController/PublicHeader.h>
+
+
